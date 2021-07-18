a74bd80e831941b72a4fe4ee448e26a47d263d94
luckzman <lumpsey@gmail.com>
2021-07-18 23:30:09 +0100

    integrate codebase with prettier and eslint

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,16 +1,25 @@
-import './Alert.scss';
-import PropTypes from 'prop-types';
+import React from "react"
+import "./Alert.scss"
+import PropTypes from "prop-types"
 
-export const Alert = ({children, onClose}) => {
+export const Alert = ({ children, onClose }) => {
   return (
-    <div data-testid="alert" className="alert d-flex justify-between align-center">
+    <div
+      data-testid="alert"
+      className="alert d-flex justify-between align-center"
+    >
       {children}
-      <div className="cursor-pointer close-btn d-flex justify-center align-center" onClick={onClose}>x</div>
+      <div
+        className="cursor-pointer close-btn d-flex justify-center align-center"
+        onClick={onClose}
+      >
+        x
+      </div>
     </div>
   )
 }
 
 Alert.propTypes = {
   children: PropTypes.node,
-  onClose: PropTypes.func
+  onClose: PropTypes.func,
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,3 @@
-export const pageData = ({  data, per = 50, page = 1}) => {
-	return data.slice(per * (page - 1), per * page);
+export const pageData = ({ data, per = 50, page = 1 }) => {
+  return data.slice(per * (page - 1), per * page)
 }
